a56fcfa3c853ed0265ef4ca42971e83482d06795
Josh <josh.quintus@gmail.com>
2016-01-17 17:22:33 -0500

    chocolatey cleanup
    * Add additional fields to the nuspec.
    * Remove depreacted calls from installers
    * Update the install url

--- Stress/Chocolatey/Tools/chocolateyInstall.ps1
+++ Stress/Chocolatey/Tools/chocolateyInstall.ps1
@@ -1,17 +1,9 @@
-﻿$packageName = 'Stress.portable' # arbitrary name for the package, used in messages
-$url ="https://github.com/jquintus/Stress/releases/download/v1.0.1/bin.zip"
+﻿$packageName = 'Stress.portable' # name for the package, used in messages
+$url ="https://github.com/jquintus/Stress/releases/download/v0.0.2.1/bin.zip"
 
-try 
-{
-  $installDir = Join-Path $env:AllUsersProfile "$packageName"
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  Install-ChocolateyPath "$installDir"
-  $env:Path = "$($env:Path);$installDir"
+$installDir = Join-Path $env:AllUsersProfile "$packageName"
+Write-Host "Adding `'$installDir`' to the path and the current shell path"
+Install-ChocolateyPath "$installDir"
+$env:Path = "$($env:Path);$installDir"
 
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
-} 
-catch 
-{
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
